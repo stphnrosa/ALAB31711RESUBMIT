c90e076c95780699182dbddc54106856d711439f
stphnrosa <stphn.rosa@gmail.com>
2025-04-04 00:25:49 -0400

    Update ALAB31711RESUBMIT.ts

--- .github/workflows/ALAB31711RESUBMIT.ts
+++ .github/workflows/ALAB31711RESUBMIT.ts
@@ -81,7 +81,6 @@ class NCycle<T> {
     //   Create a new method print, which returns nothing and has a single number parameter (either optional or defaulted to 0). Use type guards and other appropriate techniques to implement print such that it logs: "This is a <make> <model> NCycle." if make and model are not arrays."This NCycle was not created properly." if neither of the above are true.
 
     print(num: number = 0): void {
-
         if (Array.isArray(this.make) && Array.isArray(this.model)) {
             let numMake = this.make[num];
             let numModel = this.model[num];
@@ -91,30 +90,52 @@ class NCycle<T> {
             } else {
                 console.log("This NCycle was not created properly.");
             }
-
-        } else if (this.make && this.model) {
+        } else if (!Array.isArray(this.make) && !Array.isArray(this.model)) {
             console.log(`This is a ${this.make} ${this.model} NCycle.`);
         } else {
             console.log("This NCycle was not created properly.");
         }
     }
-}
 //   Create a new method printAll, which returns nothing and has no parameters.
-
-//     which returns nothing and has no parameters.
 // Use type guards and appropriate techniques to implement printAll such that it logs the same statements as print, but for all matching pairs in the make and model arrays, if applicable.
 
-// printAll(): void {
-//     if(Array.isArray(this.make) && Array.isArray(this.model)) {
-//         const minLength = Math.min(this.make.length, this.model.length);
-//     for (let i = 0; i < i++) {
-//         console.log(`This is a ${this.make[i]} ${this.model[i]} NCycle.`);
-//     }
-// } else if (this.make && this.model) {
-//     console.log(`This is a ${this.make} ${this.model} NCycle.`);
-// } else {
-//     console.log("This NCycle was not created properly.");
-// }
-//     }
-// }
+printAll(): void {
+    if (Array.isArray(this.make) && Array.isArray(this.model)) {
+        const minLength = Math.min(this.make.length, this.model.length); // use shorter length to avoid index issues
+        for (let i = 0; i < minLength; i++) {
+            console.log(`This is a ${this.make[i]} ${this.model[i]} NCycle.`);
+        }
+    } else if (!Array.isArray(this.make) && !Array.isArray(this.model)) {
+        console.log(`This is a ${this.make} ${this.model} NCycle.`);
+    } else {
+        console.log("This NCycle was not created properly.");
+    }
+}
+}
+// Test your code in whatever ways you see fit. Below, we have included a block of rudimentary testing code that you can use, but you must compare this with your own expected output.
+const testCycle2 = new NCycle<string>("This", "That", 4);
+testCycle2.print();
+testCycle2.printAll();
 
+const testCycle3 = new NCycle<string>("Make", 10, 4);
+testCycle3.print(4);
+testCycle3.printAll();
+
+const makes4 = ["Volkswagon", "Tesla", "Audi"];
+const models4 = ["Passat", "Model X", "A4"];
+const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+testCycle4.print(2);
+testCycle4.printAll();
+
+const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const models5 = [1, 1, 2, 3, 5];
+const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+testCycle5.print(7);
+testCycle5.printAll();
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+add(testCycle1.make, testCycle5.model[1]);
+// Error expected here
+add(testCycle2.make, testCycle4.model[1]);
